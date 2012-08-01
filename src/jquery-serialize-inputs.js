3611023e86ac19485638dc6938b0579b89b06152
rogerz <rogerz.zhang@gmail.com>
2012-08-01 15:50:47 +0800

    Enhance serialization and add autofill support

--- src/jquery-serialize-inputs.js
+++ src/jquery-serialize-inputs.js
@@ -1,40 +1,103 @@
 /* 
-  jQuery plugin to serialize inputs to javascript object
-  https://github.com/rogerz/jquery-serialize-inputs
+   jQuery plugin to serialize inputs to javascript object
+   https://github.com/rogerz/jquery-serialize-inputs
 
-  Rogerz Zhang <rogerz.zhang@gmail.com>
-  2012-05-19
-*/
+   Rogerz Zhang <rogerz.zhang@gmail.com>
+   2012-05-19
+   */
 
 (function ($) {
-    $.fn.serializeInputs = function () {
-        var inputs = {};
-        this.find(":input[name]").each(function () {
-            var value, key = this.name, $elem = $(this);
-            if ($elem.is(":text,:radio:checked,:checkbox:checked")) {
-                value = this.value;
-            } else if ($elem.is("select")) {
-                value = $elem.find("option:selected").val();
-            } else {
-                return;
-            }
+	function parseVal(value, type) {
+		if (type == "number") {
+			return Number(value);
+		} else if (type == "string") {
+			return String(value);
+		} else if (value === "true") {
+			return true;
+		} else if (value === "false") {
+			return false;
+		} else if (value === "null") {
+			return null;
+		} else if (isNaN(Number(value))) {
+			return value;
+		} else {
+			return Number(value);
+		}
+	}
+	$.fn.serializeInputs = function(filter) {
+		var inputs = {};
+		var arrayFilter = "select[name].array,input[name].array";
+		// you may override the default selector, the element should have 'name'
+		// attribute and 'val()' method
+		filter = filter
+			|| "select,input:text,input:hidden,input:radio:checked,input:checkbox:checked";
+		this
+			.find(filter)
+			.add(this.filter(filter))
+			.filter("[name]")
+			.each(
+					function() {
+						var key = this.name, $elem = $(this), type = undefined, value = $elem
+				.val();
+			if ($elem.is(".number")) {
+				type = "number";
+			} else if ($elem.is(".string")) {
+				type = "string";
+			}
+			if ($elem.is(".array")) {
+				try {
+					inputs[key].push(parseVal(value, type));
+				} catch (e) {
+					inputs[key] = [ parseVal(value, type) ];
+				}
+			} else {
+				inputs[key] = parseVal(value, type);
+			}
+					});
+		this.find(arrayFilter).add(this.filter(arrayFilter)).each( function(){
+			// create empty array if nothing is selected
+			if (!inputs[this.name])
+			inputs[this.name] = [];
+		});
+		return inputs;
+	};
 
-            if ($elem.is(".number")) {
-                inputs[key] = Number(value);
-            } else if ($elem.is(".string")) {
-                inputs[key] = String(value);
-            } else if (value === "true") {
-                inputs[key] = true;
-            } else if (value === "false") {
-                inputs[key] = false;
-            } else if (value === "null") {
-                inputs[key] = null;
-            } else if (isNaN(Number(value))) {
-                inputs[key] = value;
-            } else {
-                inputs[key] = Number(value);
-            }
-        });
-        return inputs;
-    };
+	$.fn.autoFillInputs = function(values, callback) {
+		var filter = "select,input:text,input:radio,input:checkbox";
+		if (!values)
+			return;
+		this.find(filter).add(this.filter(filter)).each(function() {
+			var $this = $(this);
+			if (values[this.name] == undefined) {
+				return;
+			}
+			if ($this.is(":text")) {
+				$this.val(values[this.name]);
+			} else {
+				var type = undefined;
+				if ($this.is(".number")) {
+					type = "number";
+				} else if ($this.is(".string")) {
+					type = "string";
+				}
+				if ($this.is("select")) {
+					var name = this.name;
+					$this.find("option").filter( function() {
+						return parseVal(this.value,	type) === values[name];
+					}).attr("selected", true);
+				} else if ($this.is(":checkbox")) {
+					var checkbox = this;
+					checkbox.checked = false;
+					$.each(values[this.name], function(index, value) {
+						if (parseVal(checkbox.value, type) === value) {
+							checkbox.checked = true;
+						}
+					});
+				} else if ($this.is(":radio")) {
+					this.checked = (parseVal(this.value, type) === values[this.name]);
+				}
+			}
+			callback && callback(this);
+		});
+	};
 })(jQuery);
